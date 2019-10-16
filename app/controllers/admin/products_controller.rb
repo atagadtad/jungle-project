e6d7ddb45eb2d1aa968478924d9b49eff9a0d383
atagadtad <aarontagadtad@gmail.com>
2019-10-16 17:36:07 +0000

    hid confidential admin credentials

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,6 @@
 class Admin::ProductsController < ApplicationController
+  http_basic_authenticate_with name: Rails.configuration.admin[:admin_username], password: Rails.configuration.admin[:admin_password]
+
 
   def index
     @products = Product.order(id: :desc).all
