6ecafc1d3011df8263a502360f99c4d9934d7b4a
atagadtad <aarontagadtad@gmail.com>
2019-10-16 18:45:44 +0000

    finished dashboard counts in admin

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,9 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: Rails.configuration.admin[:admin_username], password: Rails.configuration.admin[:admin_password]
 
   def show
+    @products = Product.all.count
+    @category = Category.all.count
   end
+
 end
+
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,6 +2,7 @@ class ProductsController < ApplicationController
 
   def index
     @products = Product.all.order(created_at: :desc)
+    @count = @products.count
   end
 
   def show
