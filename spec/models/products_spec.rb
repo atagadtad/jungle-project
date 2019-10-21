aa1807f9196c80b71af7ece82734335341cd8b74
atagadtad <aarontagadtad@gmail.com>
2019-10-21 20:12:16 +0000

    finished integrating rspec within products_spec.rb

--- spec/models/products_spec.rb
+++ spec/models/products_spec.rb
@@ -2,6 +2,43 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do
-    # validation tests/examples here
+    cat = Category.find_or_create_by! name: 'Video Games'
+    
+    product =  cat.products.create!({
+      name: 'Super Mario', 
+      price: 79.99, 
+      quantity: 4}) 
+
+    it 'is a valid save' do
+      expect(product).to respond_to :save!
+    end
+    
+
+    it 'is not valid without a name' do
+      product.name = nil
+      expect(product).to_not be_valid
+    end
+
+    it 'is not valid without a price' do
+      product.price = nil
+      expect(product).to_not be_valid
+    end
+
+    it 'is not valid without a quantity' do
+      product.quantity = nil
+      expect(product).to_not be_valid
+    end
+
+    it 'is not valid without a category' do
+      product = Product.create({
+        name: 'Super Mario', 
+        price: 79.99, 
+        quantity: 4
+      })
+      expect(product.errors.full_messages).to include('Category can\'t be blank')
+      expect(product).to_not be_valid
+    end
+
+
   end
 end
