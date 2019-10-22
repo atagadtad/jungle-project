26d0a41945d2c26d0f9aa440f2c9ddc4a8cf9ee0
atagadtad <aarontagadtad@gmail.com>
2019-10-22 17:47:06 +0000

    added add_to_cart_spec.rb to spec/features

---
+++ spec/features/add_to_cart_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.feature "AddToCarts", type: :feature, js: true do
+  @category = Category.create! name: 'Apparel'
+
+  10.times do |n|
+    @category.products.create!(
+      name:  Faker::Hipster.sentence(3),
+      description: Faker::Hipster.paragraph(4),
+      image: open_asset('apparel1.jpg'),
+      quantity: 10,
+      price: 64.99
+    )
+  end
+end
+end
